bfbc789382992b7d9b96541e16e9b6f7ce0d352c
jjvv44 <190886728+jjvv44@users.noreply.github.com>
2025-02-02 20:09:46 -0700

    completed 01-04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+    let st = ''; // Store the repeated string
+
+    if (num < 0) {
+        return ('ERROR')
+    }
+
+    for (let i = 1; i <= num; i++) {
+        if (i === 1) {
+            st = string;
+        } else {
+            st += string; // Append the string to itself
+        }
+    }
+
+    return st;
 };
 
 // Do not edit below this line
