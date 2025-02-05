1f14924e5771c715646630f5f1a5a14f15689391
jjvv44 <190886728+jjvv44@users.noreply.github.com>
2025-02-04 20:20:16 -0700

    finished 07, 08

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let n1 = num1;
+    let n2 = num2;
+    if (typeof num1 != "number" || typeof num2 != "number" ) {
+        return 'ERROR';
+    } else if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    } else if (num1 > num2) {
+        n1 = num2;
+        n2 = num1;
+    }
+    let finalSum = 0;
+    for (i = n1; i <= n2; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let res = 0
+	for (i=0; i<a.length; i++) {
+
+    res += a[i];
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let res = 0
+	for (i=0; i<a.length; i++) {
+    if (i > 0) {
+    res = res * a[i] }
+    else res = a[0];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = a;
+  for (i=1; i<b; i++) {
+    result = result * a;
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    result = 1;
+  } else {
+    result = a;
+  };
+
+  for (i=1; i<a; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
